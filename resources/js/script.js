d06392921f41b37fbf44eeb44e545c8022ab6f34
Jessica Roberson <Jessica Roberson>
2017-08-03 18:50:27 -0400

    Fixed typos in quotes; added function at the end of script.js to automatically update the first quote to random; added back jQuery transitions.

--- resources/js/script.js
+++ resources/js/script.js
@@ -24,7 +24,7 @@ var quotes = [
     "author": "John Calvin"
   },
   {
-    "quote": "While prayer is in the making, God is searching the heart to see from what root and spirit it arises (114).",
+    "quote": "While prayer is in the making, God is searching the heart to see from what root and spirit it arises.",
     "author": "John Bunyan"
   },
   {
@@ -76,7 +76,7 @@ var quotes = [
     "author": "Charles Spurgeon"
   },
   {
-    "quote": "So when the devil throws your sins in your face and declares that you deserve death and hell, tell h \"I admit that I deserve death and hell, what of it? For I know One who suffered and made satisfaction on my behalf. His name is Jesus Christ, Son of God, and where He is there I shall be also!\"",
+    "quote": "So when the devil throws your sins in your face and declares that you deserve death and hell, tell him \"I admit that I deserve death and hell, what of it? For I know One who suffered and made satisfaction on my behalf. His name is Jesus Christ, Son of God, and where He is there I shall be also!\"",
     "author": "Martin Luther"
   },
   {
@@ -104,7 +104,7 @@ var quotes = [
     "author": "Octavius Winslow"
   },
   {
-    "quote": "If God loves a person just the way they are, then what is the need for Jesus? Jesus would fail every single evangelism course in Seminary that's out there. He turns to a crowd of thousands and says, \"If you don't hate your life and come to die, don't come. If you do not come to die and rise again, DON'T COME!\"",
+    "quote": "If God loves a person just the way they are, then what is the need for Jesus? Jesus would fail every single evangelism course in seminary that's out there. He turns to a crowd of thousands and says, \"If you don't hate your life and come to die, don't come. If you do not come to die and rise again, DON'T COME!\"",
     "author": "Jeff Durbin"
   },
   {
@@ -120,7 +120,7 @@ var quotes = [
     "author": "John Calvin"
   },
   {
-    "quote": "Election guarantees that evangelism will result in genuine converstions.",
+    "quote": "Election guarantees that evangelism will result in genuine conversions.",
     "author": "R. B. Kuiper"
   },
   {
@@ -156,7 +156,7 @@ var quotes = [
     "author": "Charles Spurgeon"
   },
   {
-    "quote": "Every many loves the mercies of God, but a saint loves the God of his mercies.",
+    "quote": "Every man loves the mercies of God, but a saint loves the God of his mercies.",
     "author": "John Flavel"
   },
   {
@@ -172,7 +172,7 @@ var quotes = [
     "author": "John Flavel"
   },
   {
-    "quote": "The Christian is a man who expects nothing from this world. He does not pin his hopes on it. Because he knows that it is doomed.",
+    "quote": "The Christian is a man who expects nothing from this world. He does not pin his hopes on it because he knows that it is doomed.",
     "author": "D. Martyn Lloyd-Jones"
   },
   {
@@ -202,27 +202,32 @@ var quotes = [
   {
     "quote": "It is the loss of your first love that makes you seek the comfort of your bodies instead of the prosperity of your souls.",
     "author": "Charles Spurgeon"
+  },
+  {
+    "quote": "God helps those who cannot help themselves.",
+    "author": "Charles Spurgeon"
   }
     
 ];
 
 
+function generateQuote() {
+    var randomNumber = Math.floor(Math.random() * quotes.length);
+    var quote = quotes[randomNumber];
+//    document.getElementById("quote").textContent = quote.quote;
+//    document.getElementById("author").textContent = quote.author;
+    $("#quote, #author").fadeOut(750, function(){
+        $("#quote").text(quote.quote);
+        $("#author").text(quote.author);
+        $(this).fadeIn(750);
+    });
+}
 
-    function generateQuote() {
-        var randomNumber = Math.floor(Math.random() * quotes.length);
-        var quote = quotes[randomNumber];
-        document.getElementById("quote").textContent = quote.quote;
-        document.getElementById("author").textContent = quote.author;
-//        $("#quote, #author").fadeOut(750, function(){
-//            $("#quote").text(quote.quote);
-//            $("#author").text(quote.author);
-//            $(this).fadeIn(750);
-//        });
-    }
+function tweetThis() {
+    var url = "https://twitter.com/intent/tweet";
+    var text = document.getElementById('quote').textContent;
+    var author = document.getElementById('author').textContent;
+    window.open(url+"?text=" + "\"" + text + "\"" + " " + author);
+}
 
-    function tweetThis() {
-        var url = "https://twitter.com/intent/tweet";
-        var text = document.getElementById('quote').textContent;
-        var author = document.getElementById('author').textContent;
-        window.open(url+"?text=" + "\"" + text + "\"" + " " + author);
-    }
+generateQuote();
